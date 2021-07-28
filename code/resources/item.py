5e7306368aaba88426509a0c1a46748c0f0804fd
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-07-27 18:31:29 -0700

    implement SQLAlchemy into code

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -11,7 +10,10 @@ class Item(Resource):
                         required=True,
                         help="This field cannot be left empty"
                         )
-
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help="Every item needs a store id.")
     @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
@@ -25,52 +27,39 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
-            ItemModel.insert(item)
+            ItemModel.save_to_db(item)
         except:
             return {"message": "Error occured inserting the item"}, 500
 
         return item.json(), 201
 
     def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
-        connection.commit()
-        connection.close()
-        return {'message': f"{name} has been deleted"}
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return{"message": f"{name} has been deleted"}
+        return {"message": "Item not found"}
 
     def put(self, name):
         data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
-        if item is None:
-            try:
-                updated_item.insert()
-            except:
-                return {"message": "Error occured inserting the item"}, 500
-        else:
-            try:
-                updated_item.update()
-            except:
-                return {"message": "Error occured updating the item"}, 500
 
-        return updated_item.json()
+        if item is None:
+            item = ItemModel(name, **data)
+        else:
+            item.price = data['price']
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "Error occured inserting the item"}, 500
+
+        return item.json()
 
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({"name": row[0], "price": row[1]})
-        connection.close()
-        return {"items": items}
+        return {"items": [item.json() for item in ItemModel.query.all()]}
+        # return {"items": list(map(lambda x:x.json(), ItemModel.query.all()))}
--- code/resources/user.py
+++ code/resources/user.py
@@ -1,5 +1,5 @@
-import sqlite3
 from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
 from models.user import UserModel
 
 
@@ -8,17 +8,17 @@ class UserRegister(Resource):
     parser.add_argument('username', type=str, required=True)
     parser.add_argument('password', type=str, required=True)
 
+
     def post(self):
         data = UserRegister.parser.parse_args()
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(**data)
+        user.save_to_db()
 
         return {"message": "User created success"}, 201
+
+class UserList(Resource):
+    @jwt_required()
+    def get(self):
+        return {"users":[user.json() for user in UserModel.query.all()]}
